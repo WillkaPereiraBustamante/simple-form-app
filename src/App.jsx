4821063c3a9bcd8357682fd557d0c43faba4b2ec
Willka <Willka@Host-007.lan>
2024-11-14 16:00:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import Footer from "./assets/components/Footer";
+
+import './App.css'
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [showPasswordError, setShowPasswordError] = useState(false);
+  const [finalMessage, setFinalMessage] = useState(false);
+  
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    if (password.length < 8) {
+      setShowError(true);
+    } else if (password !== passwordCheck) {
+      setShowPasswordError(true);
+    } else {
+      setFinalMessage(true);
+
+    }
+  };
+  
+  return (
+    <>
+        <h1>Create account</h1>
+
+       <form onSubmit={handleSubmit} className="form">
+          <span>Name</span>
+        <input type="text" placeholder="Jean Dupont" name="username" value={username} onChange={(event) => {
+            setUsername(event.target.value);
+          }}/>
+          <span>Email</span>
+        <input type="email" placeholder="jeandupont@lereacteur.io" name="email" value={email} onChange={(event) => {
+            setEmail(event.target.value);
+          }}/>
+          <span>Password</span>
+        <input className={showPasswordError ? "redBorder" : ""} type="password" placeholder="azerty" name="password" value={password} onChange={(event) => {
+            setPassword(event.target.value);
+          }}/>
+          <span>Confirm your password</span>
+        <input className={showPasswordError ? "redBorder" : ""} type="password" placeholder="azerty" name="passwordCheck" value={passwordCheck} onChange={(event) => {
+            setPasswordCheck(event.target.value);
+          }}/>
+        <button type="submit">Register</button>
+      </form>
+
+      {showError && (
+        <p style={{ color: "red" }}>
+          Votre mot de passe doit faire plus de 8 caractères !
+        </p>
+      )}
+      {showPasswordError && (
+        <p style={{ color: "red" }}>
+          Les mots de passe ne sont pas identiques !
+        </p>
+      )}
+      {finalMessage && (
+        <div className="result">
+          <div>
+        <h1>Results</h1>
+          <div className="resultContent">
+            <div>name : {username}</div>
+            <div>email : {email}</div>
+            <div>password : {password}</div>
+          </div>
+          <button>Edit your informations</button>
+          </div>
+        </div>
+      )}
+
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  
+    return <>
+        <div className="footer">
+            <p>Made with <a href="https://react.dev/">React</a> at <a href="https://www.lereacteur.io/?utm_source=google&utm_medium=cpc&utm_campaign=FR-SN-Brand&utm_term=le%20r%C3%A9acteur&utm_content=679341333814&gad_source=1&gclid=Cj0KCQiA88a5BhDPARIsAFj595hJzR11x2edvxFeJ2Tn5ajgZxdNxeSfsvenESLSIqIKJpzvi0jk8n8aAoY6EALw_wcB">Le Reacteur</a> By <a href="https://github.com/WillkaPereiraBustamante">Willka PEREIRA BUSTAMANTE</a></p>
+        </div>
+    </>
+    ;
+  };
+
+export default Footer;
+  
